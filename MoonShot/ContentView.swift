34cd4c6080f760df5236875c6dd1b6574c788ff3
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-30 15:58:09 +0530

    adding the custom label in navigation link.

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack{
+            Spacer()
             NavigationLink("Tap me"){
                 Text("Detail View")
             }
-                .navigationTitle("SwiftUI ")
+            Spacer()
+            NavigationLink{
+                Text("Detail View")
+            } label:{
+                VStack{
+                    Text("Add new screen")
+                    Text("Custom Label")
+                    Image(systemName: "face.smiling")
+                }.font(.largeTitle)
+                
+            }
+            .navigationTitle("SwiftUI ")
+            Spacer()
         }
     }
 }
