eaea4b6712ffe4d02472e92aa24f623e65ce5c26
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-30 15:19:00 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Mohit Sengar on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
