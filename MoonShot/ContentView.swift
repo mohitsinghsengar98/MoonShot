79a8836dfeaddab2c2d2ebcee05c32e93efcd33a
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-30 15:55:09 +0530

    Creating the navigationstack and navigation link to show new screen (more details) of selected item.

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -7,27 +7,13 @@
 
 import SwiftUI
 
-struct CustomText:View {
-    let text : String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(text:String){
-        print("creating new custom text")
-        self.text = text
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal){
-            LazyHStack(spacing:10){ // lazy will have more space than regular HStack or VStack and it will load the view that is need to present.
-                ForEach(0..<100){
-                    CustomText(text:"Item \($0)").font(.title)
-                }
-            }.frame(maxWidth: .infinity)
+        NavigationStack{
+            NavigationLink("Tap me"){
+                Text("Detail View")
+            }
+                .navigationTitle("SwiftUI ")
         }
     }
 }
