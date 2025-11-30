524d923161c01dd6f3c9cb1931a7ff964ec74ca8
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-30 21:57:49 +0530

    New view to show the details of the mission.

---
+++ MoonShot/MissionView.swift
@@ -0,0 +1,40 @@
+//
+//  MissionView.swift
+//  MoonShot
+//
+//  Created by Mohit Sengar on 30/11/25.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission : Mission
+    
+    var body: some View {
+        ScrollView{
+            VStack{
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal){
+                        width, axis in width * 0.6
+                    }
+                VStack(alignment: .leading){
+                    Text("Mission Highlights").font(.title.bold()).padding(.bottom, 5)
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }.navigationTitle(mission.displayName)
+            .navigationBarTitleDisplayMode(.inline)
+            .background(.darkBackground)
+            
+    }
+}
+
+#Preview {
+    let missions:[Mission] = Bundle.main.decode("missions.json")
+   return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
